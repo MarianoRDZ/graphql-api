02246658ee8bae7b5bbed2baff5073d1dc8e879b
Mariano Rodriguez <mariano.rdz@gmail.com>
2022-11-22 00:47:40 -0300

    partial changes

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!")
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const app = express();
+
+app.listen(3000);
+console.log("Server is running on port 3000");
