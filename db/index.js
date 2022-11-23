28b992c10627cb4b701d6e3656f6aa84fa6534ba
Mariano Rodriguez <mariano.rdz@gmail.com>
2022-11-23 15:11:30 -0300

    Added better log

--- db/index.js
+++ db/index.js
@@ -5,9 +5,8 @@ const USERNAME = process.env.MONGODB_USERNAME
 const PASSWORD = process.env.MONGODB_PASSWORD
 
 const connectDb = async () => {
-  console.log('Trying to connect')
   await mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.i6cr03m.mongodb.net/test`)
-  console.log('Connected!')
+  console.log('MongoDB connection established')
 }
 
 module.exports = { connectDb }
